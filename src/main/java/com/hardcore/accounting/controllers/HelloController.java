ea415f1a3cdb2b0db4530ea6fa1d134c6cccaf79
liudan <258829495@qq.com>
2021-09-16 06:39:13 +0800

    feat(Controller):Implement the hello controller

---
+++ src/main/java/com/hardcore/accounting/controllers/HelloController.java
@@ -0,0 +1,23 @@
+package com.hardcore.accounting.controllers;
+
+import com.hardcore.accounting.model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloController {
+    private AtomicLong counter =new AtomicLong();//定义一个计数器,incrementAndGet()先加后取
+
+
+   // @RequestMapping(path="v1/greeting",method = RequestMethod.GET)
+    //但是每次都要输入如上method有点繁琐，做如下改进
+    @GetMapping("v1/greeting")
+    public Greeting sayHello(@RequestParam("name") String name){
+        return new Greeting(counter.incrementAndGet(), String.format("Hello, %s",name));
+
+    }
+}
